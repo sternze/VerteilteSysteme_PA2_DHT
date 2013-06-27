dfcf29091080c869042e0a33a5a0a3116f011e0b
Daniel <sternze@gmail.com>
2013-06-27 22:05:39 +0200

    batch file kacke

---
+++ sixNodesIncludingGUI.cmd
@@ -0,0 +1,31 @@
+taskkill.exe /F /IM java.exe /T
+
+call ant clean
+call ant build
+
+
+set countOfNodes=3
+
+set MyIp=143.205.195.181
+
+rem for /f "usebackq tokens=13 delims=: " %%i in ('ipconfig ^| find "IPv4 Address" ') do set MyIp=%%i
+set GuiIp=%MyIp%
+set ConnectingNodeIp=%MyIp%
+
+rem the command 'rem' is just for commenting lines out
+rem set GuiIp=143.205.192.146
+rem set ConnectingNodeIp=143.205.192.146
+
+start cmd /C "ant runGui -DMyIp=%MyIp%"
+
+timeout 1
+start cmd /C "ant runNode -DMyIp=%MyIp%"
+
+
+:loop
+
+timeout 1
+start cmd /C "ant runNode -DServiceName=PA2_MyKV -DNodeIP:Port=%ConnectingNodeIp%:8000 -DGraphViewIP:Port=%GuiIp%:7998 -DMyIp=%MyIp%"
+
+set /a countOfNodes-=1
+if %countOfNodes% GTR 1 goto loop
--- sixNodesIncludingGUI_manual_Id.cmd
+++ sixNodesIncludingGUI_manual_Id.cmd
@@ -1,11 +1,12 @@
 taskkill.exe /F /IM java.exe /T
 
+call ant clean
 call ant build
 
 
-set countOfNodes=5
+set countOfNodes=3
 
-set MyIp=143.205.199.224
+set MyIp=143.205.195.181
 
 rem for /f "usebackq tokens=13 delims=: " %%i in ('ipconfig ^| find "IPv4 Address" ') do set MyIp=%%i
 set GuiIp=%MyIp%
@@ -18,13 +19,9 @@ rem set ConnectingNodeIp=143.205.192.146
 start cmd /C "ant runGui -DMyIp=%MyIp%"
 
 timeout 1
-start cmd /C "ant runNode -DMyIp=%MyIp%"
+start cmd /C "ant runNode -DMyIp=%MyIp% -DmanualID=0"
 
-
-:loop
-
-timeout 1
-start cmd /C "ant runNode -DServiceName=PA2_MyKV -DNodeIP:Port=%ConnectingNodeIp%:8000 -DGraphViewIP:Port=%GuiIp%:7998 -DMyIp=%MyIp%"
-
-set /a countOfNodes-=1
-if %countOfNodes% GTR 1 goto loop
+FOR %%G IN (1,3) DO (
+	timeout 1
+	start cmd /C "ant runNode -DServiceName=PA2_MyKV -DNodeIP:Port=%ConnectingNodeIp%:8000 -DGraphViewIP:Port=%GuiIp%:7998 -DMyIp=%MyIp% -DmanualID=%%G"
+)
